1a30eea740aab26a846a5b09bc9f1c61f337a799
EricAlexanderG <EricAlexanderG@gmail.com>
2016-06-16 10:22:45 -0700

    final weed-outs of merge

--- src/index.js
+++ src/index.js
@@ -7,22 +7,18 @@ import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import configureStore from './store/configureStore';
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
-<<<<<<< HEAD
+
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-=======
->>>>>>> d549bd0244da408a0d970dba3843951a156fa468
+
 
 const store = configureStore();
 
 render(
   <Provider store={store}>
-<<<<<<< HEAD
     <MuiThemeProvider muiTheme={ getMuiTheme() }>
     <Router history={browserHistory} routes={routes} />
     </MuiThemeProvider>
-=======
     <Router history={browserHistory} routes={routes} />
->>>>>>> d549bd0244da408a0d970dba3843951a156fa468
   </Provider>, document.getElementById('app')
 );
